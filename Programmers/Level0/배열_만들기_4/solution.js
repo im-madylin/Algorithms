4df8f6c93f5122c536bbd8a133c4c2b0db8345a3
im-madylin <im.madylin@gmail.com>
2024-06-05 15:23:36 +0900

    배열 만들기기 4

---
+++ Programmers/Level0/배열_만들기_4/solution.js
@@ -0,0 +1,16 @@
+function solution(arr) {
+    var stk = [];
+    
+    for (let i = 0; i < arr.length; ) {
+        if (stk.length === 0) {
+            stk.push(arr[i++]);
+        }
+        else if (stk[stk.length - 1] < arr[i]) {
+            stk.push(arr[i++]);
+        }
+        else {
+            stk.pop();
+        }
+    }
+    return stk;
+}
